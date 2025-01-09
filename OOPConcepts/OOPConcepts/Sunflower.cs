a0f4c2df4acc085083b6602945de2db940ffe356
YerayRodriguez <yeray.rodriguez.7e5@itb.cat>
2025-01-09 20:42:38 +0100

    UML Implemented

---
+++ OOPConcepts/OOPConcepts/Sunflower.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOPConcepts
+{
+    public class Sunflower : Flower, IEdible
+    {
+        public int Seeds { get; set; }
+        public Sunflower(string name, string color, int seeds) : base(name, color)
+        {
+            Seeds = seeds;
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine($"You can eat the sunflower {Name}'s seeds.");
+        }
+
+        public override void Grow()
+        {
+            Console.WriteLine($"The sunflower {Name} is growing beautifully.");
+        }
+
+        public override void Photosynthesize()
+        {
+            Console.WriteLine($"The sunflower {Name} is photosynthesizing");
+        }
+    }
+
+}
